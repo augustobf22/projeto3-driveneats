a7027ba3b0009524b96ff77d61a066edd49bd55a
augustof <augustobfanck@gmail.com>
2023-04-01 12:41:40 -0300

    only bonus & data-tests missing

--- scripts.js
+++ scripts.js
@@ -75,3 +75,65 @@ function selectDessert(thisBox) {
 
     updateButton();
 }
+
+function getFood() {
+    const foodSelected = document.querySelector('.box-food.greenBorder');
+    
+    if(foodSelected !== null) {
+        const foodSelectedText = foodSelected.querySelector('h4').innerHTML;
+        return(foodSelectedText);
+    }
+}
+
+function getDrink() {
+    const drinkSelected = document.querySelector('.box-drink.greenBorder');
+    
+    if(drinkSelected !== null) {
+        const drinkSelectedText = drinkSelected.querySelector('h4').innerHTML;
+        return(drinkSelectedText);
+    }
+}
+
+function getDessert() {
+    const dessertSelected = document.querySelector('.box-dessert.greenBorder');
+    
+    if(dessertSelected !== null) {
+        const dessertSelectedText = dessertSelected.querySelector('h4').innerHTML;
+        return(dessertSelectedText);
+    }
+}
+
+function getTotal() {
+    const foodSelected = document.querySelector('.box-food.greenBorder');
+    const foodSelectedPrice = foodSelected.querySelector('h6').innerHTML;
+    
+    const drinkSelected = document.querySelector('.box-drink.greenBorder');
+    const drinkSelectedPrice = drinkSelected.querySelector('h6').innerHTML;
+    
+    const dessertSelected = document.querySelector('.box-dessert.greenBorder');
+    const dessertSelectedPrice = dessertSelected.querySelector('h6').innerHTML;
+
+    const foodPrice = Number(foodSelectedPrice.replace(/\D/g, ''));
+    const drinkPrice = Number(drinkSelectedPrice.replace(/\D/g, ''))
+    const dessertPrice = Number(dessertSelectedPrice.replace(/\D/g, ''));
+
+    const totalCalc = (foodPrice+drinkPrice+dessertPrice) / 100;
+
+    return totalCalc.toFixed(2);
+}
+
+function sendWpp(/*const name, const address*/) {
+    const whichFood = getFood();
+    const whichDrink = getDrink();
+    const whichDessert = getDessert();
+    const total = getTotal();
+    
+    const bruteText="Olá, gostaria de fazer o pedido: \n- Prato: "+whichFood+"\n- Bebida: "+whichDrink+"\n- Sobremesa: "+whichDessert+"\nTotal: R$ "+total;
+    const msgWpp = encodeURIComponent(bruteText);
+    
+    window.location='https://wa.me/5551982420112?text='+msgWpp;
+}
+
+/*function closeOrder() {
+    prompt
+}*/
